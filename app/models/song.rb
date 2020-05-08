eff6ab5d146b9bd7702ed39189e99a379f029bc7
Joshua Tan <jtsarvida@gmail.com>
2020-05-08 16:45:11 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    artist.name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    artist.name =
   end
 end
